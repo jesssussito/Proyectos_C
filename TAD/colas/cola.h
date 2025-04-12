fb804f43dfbd51b3f7b0d8c6eb12337cd39d699c
Jesssussito <chapvaliente@gmail.com>
2025-04-12 13:57:59 +0200

    Mantener solo la carpeta TAD_II en la rama TAD_II

--- TAD/colas/cola.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLA_H
-#define COLA_H
-#include "../listas/lista.h"
-int createTail(list *tail);
-int enqueue(elementType x,list *tail);
-int dequeue(list *tail);
-elementType front(list *tail);
-int isEmptyTail(list *tail);
-#endif
